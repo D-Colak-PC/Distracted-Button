4609bec6ddedfccfbbfe14b1bfd7f6bcdb2637d9
Dennis Colak <dennis.colak@pinecrest.edu>
2025-02-17 10:54:14 -0500

    added plus and minus buttons

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 count = 0;
 const increaseCounter = () => {
     count++;
+    updatePage();
+}
+
+const decreaseCounter = () => {
+    count = Math.max(0, count - 1);
+    updatePage();
+}
+
+const updatePage = () => {
     document.getElementById("counter").innerHTML = count.toString();
 }
